c080e1b4c1452958ba392c66d0395fbe25fb4d57
Harjot Singh <harjjotsinghh@gmail.com>
2023-09-01 17:41:53 +0530

    Added custom font

--- frontend/swapify/tailwind.config.ts
+++ frontend/swapify/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily : {
+        'majorMono' : ["MajorMono", 'monospace'],
+        'inconsolata': ["Inconsolata", "monospace"]
+      }
     },
   },
   plugins: [],
